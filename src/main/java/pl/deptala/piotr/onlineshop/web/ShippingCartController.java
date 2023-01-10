03c58ed45d7c689984fbeae8644a49a6c8009b55
Piotr <deptala.piotr@gmail.com>
2023-01-10 20:57:54 +0100

    implement CRUD method in ShoppingCartController class

--- src/main/java/pl/deptala/piotr/onlineshop/web/ShippingCartController.java
+++
@@ -1,4 +0,0 @@
-package pl.deptala.piotr.onlineshop.web;
-
-public class ShippingCartController {
-}
--- src/main/java/pl/deptala/piotr/onlineshop/web/model/ShoppingCartModel.java
+++ src/main/java/pl/deptala/piotr/onlineshop/web/model/ShoppingCartModel.java
@@ -1,16 +1,21 @@
 package pl.deptala.piotr.onlineshop.web.model;
 
+
+import java.util.List;
+
 public class ShoppingCartModel {
 
     private Long id;
     private UserModel user;
+    private List<ProductModel> productModels;
 
     public ShoppingCartModel() {
     }
 
-    public ShoppingCartModel(Long id, UserModel user) {
+    public ShoppingCartModel(Long id, UserModel user, List<ProductModel> productModels) {
         this.id = id;
         this.user = user;
+        this.productModels = productModels;
     }
 
     public Long getId() {
@@ -29,11 +34,20 @@ public class ShoppingCartModel {
         this.user = user;
     }
 
+    public List<ProductModel> getProductModels() {
+        return productModels;
+    }
+
+    public void setProductModels(List<ProductModel> productModels) {
+        this.productModels = productModels;
+    }
+
     @Override
     public String toString() {
-        return "ShoppingCartEntity{" +
+        return "ShoppingCartModel{" +
                 "id=" + id +
                 ", user=" + user +
+                ", productModels=" + productModels +
                 '}';
     }
 }
